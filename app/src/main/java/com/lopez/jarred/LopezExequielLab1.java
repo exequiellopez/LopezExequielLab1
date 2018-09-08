083018e9d95866f9bb4df1abff73535b9a04452b
Jarred <2015081642@ust-ics.mygbiz.com>
2018-09-08 23:07:30 +0800

    Initial commit

---
+++ app/src/main/java/com/lopez/jarred/LopezExequielLab1.java
@@ -0,0 +1,13 @@
+package com.lopez.jarred;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LopezExequielLab1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lopez_exequiel_lab1);
+    }
+}
